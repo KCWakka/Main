2340683df2b9f71d9ef1b71aa874b82cf60a25a5
BT_2N2_05 <sample@gmail.com>
2024-05-10 13:54:24 -0400

    done

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class GraphicsPanel {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GraphicsPanel extends JPanel {
+    private double stringX;
+    private double starY;
+    private BufferedImage image;
+    public GraphicsPanel () {
+        stringX = 300;
+        starY = 350;
+        try {
+            image = ImageIO.read(new File("src/download.jpg"));
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        stringX -= 0.2;
+        if (stringX < 50) {
+            stringX = 300;
+        }
+        starY += 0.4;
+        if (starY > 500) {
+            starY = 0;
+        }
+        g.setColor(Color.RED);
+        g.drawOval(20, 30, 100, 300);
+        g.setColor(Color.BLUE);
+        g.fillOval(25, 30, 90, 300);
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Times new Roman", Font.ITALIC, 24));
+        g.drawString("Bob the builder, can we build it!",50, (int)starY);
+        g.drawImage(image, (int) stringX, 50, null);
+        g.draw3DRect(400, 200, 100, 200, true);
+    }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame implements Runnable{
+    private GraphicsPanel panel;
+    public MainFrame() {
+        JFrame frame = new JFrame("Intro to Animation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 500);
+        frame.setLocation(300, 50);
+
+        // create and add panel
+        panel = new GraphicsPanel();
+        frame.add(panel);
+
+        //display frame
+        frame.setVisible(true);
+
+        // start thread, required for animation
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+    @Override
+    public void run() {
+        while (true) {
+            panel.repaint();
+        }
+    }
 }
